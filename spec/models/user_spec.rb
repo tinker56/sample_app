9b83bb9385d009eaef61105410a28b8d4b127f6e
tinker <tinker.taylor7@gmail.com>
2014-06-28 09:53:14 +0800

    Added save emails to lowercase

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -60,6 +60,16 @@ describe User do
     it {should_not be_valid}
   end
 
+  describe "email addresses with mixed case" do
+    let(:mixed_case_email) { "Foo@ExAPLE.CoM" }
+
+    it "should be saved as all lower-case" do
+      @user.email = mixed_case_email
+      @user.save
+      expect(@user.reload.email).to eq mixed_case_email.downcase
+    end
+  end
+
   describe "when password is not present" do
     before do
       @user = User.new(name: "Example User", email: "user@example.org",
